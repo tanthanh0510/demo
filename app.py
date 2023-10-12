a178679c0544d7092f74f04f264ce288549f4f37
tanthanh <trantanthanh0510@gmail.com>
2023-10-12 14:14:56 +0700

    add js

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 import os
 import io
 import base64
 from PIL import Image
-from model import generate_caption
+# from model import generate_caption
 
 app = Flask(__name__)
 
@@ -29,11 +29,26 @@ def upload_file():
         image.save(os.path.join("uploads", file.filename))
         encoded_image = base64.b64encode(
             image_bytes.getvalue()).decode('utf-8')
-        text = generate_caption(os.path.join("uploads", file.filename))
-        return render_template('index.html', encoded_image=encoded_image, text=text)
-    else:
-        return "Invalid file format. Allowed formats: png, jpg, jpeg, gif, dicom"
+        # text = generate_caption(os.path.join("uploads", file.filename))
+        finding = "Finding"
+        impress = "Impression"
+        tmp = {"image": encoded_image,
+               "captions":
+               [{
+                   "Finding": "Finding",
+                "Impression": "Impression",
+                "p": 0.78,
+                }, {
+                   "Finding": "Finding",
+                   "Impression": "Impression",
+                   "p": 0.8,
+               }]}
+        # text = ["123", "456", "789"]
+        return jsonify(tmp)
+    #     return render_template('index.html', encoded_image=encoded_image, text=text)
+    # else:
+    #     return "Invalid file format. Allowed formats: png, jpg, jpeg, gif, dicom"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
